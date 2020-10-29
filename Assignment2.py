3be244640fe1787d3e4c056df8817b77de46f50e
maxicor <71939783+maxicor@users.noreply.github.com>
2020-10-29 19:37:07 +0530

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -25,12 +25,13 @@ database = {
     {
     "account_name": 'shubham kalani',
     "age":21,
-    "email":'shubhamkalanni21@gmail.com',
+    "email":'shubhamkalani21@gmail.com',
     'password':'shubham567',
     'balance':55540
     }
 } 
 def login_menu():
+    print("press 0. balance check")
     print("press 1. for deposit")
     print("press 2. for withdrawl")
     print("press 3. for password change")
@@ -38,59 +39,63 @@ def login_menu():
     print("press 5. for exit")
 
 def login():
-    account_no=input()
+    account_no=input('Enter account number ')
     if account_no in database:
-    	passw=input()
-    	if passw == database[account_no]['password']:
-    		print('welcome',database[account_no]['account_name'])
-    		while true:
+        passw=input("Enter password ")
+        if passw == database[account_no]['password']:
+            print('welcome',database[account_no]['account_name'])
+            while True:
                 login_menu()
                 ch=int(input('enter the choice'))
-                if(ch==1):
-                     deposit=int(input())
-                     database[account_no]['balance']+=deposit
+                if (ch == 0):
+                    print(database[account_no]['balance'])
+                elif(ch==1):
+                    deposit=int(input("Enter deposit ammount"))
+                    database[account_no]['balance']+=deposit
                 elif(ch==2): 
-                	withdrawl=int(input())
-                	database[account_no]['balance']-=withdrwal
+                    withdrawl=int(input("Enter withdrawl ammount"))
+                    database[account_no]['balance']-=withdrawl
                 elif(ch==3):
-                	update_pass = input()
-                	database[account_no]['password'] = update_pass
-                elif(ch==4):#forget passs
-                    #verify your email addresss
-                    verify_email = input()
-                    if verfiy_email == database[account_no]['email']:
-                    	forget_pass = input('new pass')
+                    update_pass = input("Enter new password")
+                    database[account_no]['password'] = update_pass
+                elif(ch==4):
+                    verify_email = input("Enter verifying email")
+                    if verify_email == database[account_no]['email']:
+                        forget_pass = input('new pass')
                         database[account_no]['password'] = forget_pass
                 elif(ch==5):
-                	break
+                        break
                 else:
-                	print("enter the valid key")
-                  login_menu()
+                    print("enter the valid key")
+                    login_menu()
             
     else:
         print('not valid')
 
 def start_menu():
-	print('1. for login')
+    print('1. for login')
     print('2. sign up')
     print('3. exit')
 
 def menu():
-    while true:
+    while True:
         start_menu()
         choice=int(input())
         if(choice==1):
             login()
-        elif(ch==2):
-            account_no = input()
-            account_name = input()
-            age = int(input())
-            email = input()
-            passw = input()
-            open_bal = int(input())
-            database[account_no]={"account_name":account_name,"age":age,"email":email,"password":passw,"balance":open_bal}#crate a new dict in      
+        elif(choice==2):
+            account_no = input("Enter Account number")
+            account_name = input("Enter Account name")
+            age = int(input("Enter Age"))
+            email = input("Enter email")
+            passw = input("Enter password")
+            open_bal = int(input("Enter opening balance"))
+            database[account_no]={"account_name":account_name,"age":age,"email":email,"password":passw,"balance":open_bal}  
         elif(choice==3):
-            break
+            return 
         else:
             print('enter the valid key')
-            login()
+    
+
+
+menu()
